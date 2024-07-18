7d10a5ea2e36dd602b9032f65a74f5f34f32f9c1
Manase44 <fikopersempre@gmail.com>
2024-07-18 12:11:05 +0300

    implement the delete user controller

---
+++ server/controllers/user/delete.controller.js
@@ -0,0 +1,29 @@
+import {PrismaClient} from '@prisma/client';
+const prisma = new PrismaClient();
+
+const deleteUser = async(req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const confirmExistence = await prisma.user.findUnique({
+            where:{
+                id:userId
+            }
+        })
+        if (confirmExistence) {
+            return res.status(404).json({ok:false, message:"The user does not exist"})
+        }
+        const deletedUser = await prisma.user.delete({
+            where:{
+                id:userId
+            }
+        })
+        if (deleteUser) {
+            return res.status(200).json({ok:true, message:"The user was deleted successfully"})
+        }
+    } catch (error) {
+        return res.status(500).json({ok:false, message: error.message})
+    }
+}
+
+export default deleteUser;
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import registerUser from "../controllers/user/register.controller.js";
+import deleteUser from "../controllers/user/delete.controller.js";
 const route = Router();
 
 route
 .post("/register", registerUser)
+.delete("/:id", deleteUser)
 
 export default route;
