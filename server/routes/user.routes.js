dbb6494f3159f3ddf87fada73e1e1205a03f4d78
Manase44 <fikopersempre@gmail.com>
2024-07-18 12:26:16 +0300

    implement the get a specific user controller

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import registerUser from "../controllers/user/register.controller.js";
 import deleteUser from "../controllers/user/delete.controller.js";
 import getAllusers from "../controllers/user/getUsers.controller.js";
+import getSpecificUser from "../controllers/user/getSpecificUser.controller.js";
 const route = Router();
 
 route
 .get("/", getAllusers)
+.get("/:id", getSpecificUser)
 .post("/register", registerUser)
 .delete("/:id", deleteUser)
 
