ba3e9e72b2c454607fced7e0340d197161a27273
Manase44 <fikopersempre@gmail.com>
2024-07-20 22:05:25 +0300

    implement the footer

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='footer-section'>
+        <p>&copy; {new Date().getFullYear()} mutmee by gunga</p>
+    </footer>
+  )
+}
+
+export default Footer
