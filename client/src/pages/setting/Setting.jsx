1875205789322a9c9e21fb523e36cc15b02a2450
Manase44 <fikopersempre@gmail.com>
2024-07-22 04:57:23 +0300

    romove the update user details form to a separate component

--- client/src/pages/setting/Setting.jsx
+++ client/src/pages/setting/Setting.jsx
@@ -2,32 +2,21 @@ import { useState } from "react";
 import "./Setting.css";
 import post from "../../assets/post.jpg";
 import { Link } from "react-router-dom";
-import { useFormik } from "formik";
+
 import themeColorStore from "../../store/themeColor.store";
-import userDetailsStore from "../../store/currentUser.store";
 import authenticatedStore from "../../store/authenticated.store";
+import UpdateProfile from "./UpdateProfile";
 
 const Setting = () => {
   const { theme, setTheme } = themeColorStore();
-  const user = userDetailsStore((state) => state.user);
   const setIsAuthenticated = authenticatedStore((state) => state.setIsAuthenticated)
   const [general, setGeneral] = useState(true);
   const [editProfile, setEditProfile] = useState(false);
   const [changePassword, setChangePassword] = useState(false);
   const [account, setAccount] = useState(false);
-  const [editWebsite, setEditWebsite] = useState(false);
-  const [editBio, setEditBio] = useState(false);
-  const [editRole, setEditRole] = useState(false);
+  
 
-  const userDetailsUpdateForm = useFormik({
-    initialValues: {
-      imageUrl: "",
-      bio: "",
-      website: "",
-      phoneNumber: "",
-      role: "",
-    },
-  });
+  
   return (
     <div className="setting-page-container">
       <div className="setting-menu">
@@ -97,67 +86,7 @@ const Setting = () => {
           <div className="selected-setting-container">
             <h4>edit profile</h4>
 
-            <form onSubmit={userDetailsUpdateForm.handleSubmit} className="selected-setting-container">
-            <div className="edit-image-container">
-              <div className="right-profile">
-                <div className="right-profile-image">
-                  {user.imageUrl && <img src={user.imageUrl} alt="current user profile" />}
-                </div>
-                <div className="right-profile-description">
-                  <p className="right-profile-username">{user.userName}</p>
-                  <span>{user.role}</span>
-                </div>
-              </div>
-              <button>{user.imageUrl ? "change photo" : "add photo"}</button>
-            </div>
-
-            <div className="edit-role-container">
-              <p className="setting-section-para">role:</p>
-              <div className="input-wrapper">
-                <input type="text" name="role" id="role" value={user.role ? user.role : "No role specified!"} disabled={!editRole} />
-                <div className="input-icon">
-                  <Link onClick={() => { setEditRole(true) }} className="setting-edit-button">{user.role ?"edit" : "add"}</Link>
-                </div>
-              </div>
-              <p className="setting-input-little-description">Specify your role in the university for better experience.</p>
-            </div>
-
-            <div className="edit-bio-container">
-              <p className="setting-section-para">bio:</p>
-              <div className="input-wrapper">
-                <textarea
-                  name="bio"
-                  id="bio"
-                  value={user.bio ? user.bio : "No bio!"}
-                  rows={1}
-                  disabled={!editBio}
-                ></textarea>
-                <div className="input-icon">
-                  <Link onClick={() => setEditBio(true)} className="setting-edit-button">{user.bio ? "edit" : "add"}</Link>
-                </div>
-              </div>
-              <p className="setting-input-little-description">Tell us a little about yourself.</p>
-            </div>
-
-            <div className="edit-website-container">
-              <p className="setting-section-para">website:</p>
-              <div className="input-wrapper">
-                <input
-                  type="text"
-                  name="websiteUrl"
-                  id="websiteUrl"
-                  disabled={!editWebsite}
-                  value={user.website ? user.website : "No website url!"}
-                />
-                <div className="input-icon">
-                  <Link onClick={() => setEditWebsite(true)} className="setting-edit-button">{user.website ?"edit" : "add"}</Link>
-                </div>
-              </div>
-              <p className="setting-input-little-description">Portfolio / organization / personal website.</p>
-            </div>
-            <button type="submit">update</button>
-            </form>
-            
+            <UpdateProfile/>
             
           </div>
         ) : account ? (
---
+++ client/src/pages/setting/UpdateProfile.jsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import userDetailsStore from "../../store/currentUser.store";
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+
+const UpdateProfile = () => {
+    const user = userDetailsStore((state) => state.user);
+    const [editWebsite, setEditWebsite] = useState(false);
+    const [editBio, setEditBio] = useState(false);
+    const [editRole, setEditRole] = useState(false);
+    const [imageInput, setImageInput] = useState(null);
+    const [imageLink, setImageLink] = useState(null);
+
+    const userDetailsUpdateForm = useFormik({
+        initialValues: {
+            imageUrl: user.imageUrl,
+            bio: user.bio,
+            website: user.website,
+            role: user.role,
+        },
+        onSubmit: (data) => {
+            // data.imageUrl = imageLink;
+            console.log(data)
+        }
+    });
+    return (
+        <form onSubmit={userDetailsUpdateForm.handleSubmit} className="selected-setting-container">
+            <>
+                <div className="edit-image-container">
+                    <div className="right-profile">
+                        <div className="right-profile-image">
+                            {user.imageUrl && <img src={user.imageUrl} alt="current user profile" />}
+                        </div>
+                        <div className="right-profile-description">
+                            <p className="right-profile-username">{user.userName}</p>
+                            <span>{user.role}</span>
+                        </div>
+                    </div>
+                    <div className="setting-edit-photo-button-contaner">
+                        <p>{user.imageUrl ? "change photo" : "add photo"}</p>
+                        <input type="file" name="image" id="image" onChange={(e) => { setImageInput(e.target.files[0]) }} accept="image/*" />
+                    </div>
+                </div>
+                {imageInput && <span className="edit-photo-selected-image">{imageInput.name}</span>}
+            </>
+
+            <div className="edit-role-container">
+                <p className="setting-section-para">role:</p>
+                <div className="input-wrapper">
+                    <select name="role" id="role" disabled={!editRole} onChange={userDetailsUpdateForm.handleChange} value={userDetailsUpdateForm.values.role}>
+                        {!user.role && !editRole && <option value="student">No role!</option>}
+                        <option value="student">student</option>
+                        <option value="lecturer">lecturer</option>
+                        <option value="lecturer">staff</option>
+                    </select>
+                    <div className="input-icon">
+                        <Link onClick={() => { setEditRole(true) }} className="setting-edit-button">{user.role ? "edit" : "add"}</Link>
+                    </div>
+                </div>
+                <p className="setting-input-little-description">Specify your role in the university for better experience.</p>
+            </div>
+
+            <div className="edit-bio-container">
+                <p className="setting-section-para">bio:</p>
+                <div className="input-wrapper">
+                    <textarea
+                        name="bio"
+                        id="bio"
+                        placeholder={!user.bio && "No bio!"}
+                        rows={1}
+                        disabled={!editBio}
+                        value={userDetailsUpdateForm.values.bio}
+                        onChange={userDetailsUpdateForm.handleChange}
+                    ></textarea>
+                    <div className="input-icon">
+                        <Link onClick={() => setEditBio(true)} className="setting-edit-button">{user.bio ? "edit" : "add"}</Link>
+                    </div>
+                </div>
+                <p className="setting-input-little-description">Tell us a little about yourself.</p>
+            </div>
+
+            <div className="edit-website-container">
+                <p className="setting-section-para">website:</p>
+                <div className="input-wrapper">
+                    <input
+                        type="text"
+                        name="websiteUrl"
+                        id="websiteUrl"
+                        disabled={!editWebsite}
+                        placeholder={!user.website && "No website url!"}
+                        value={userDetailsUpdateForm.values.website}
+                        onChange={userDetailsUpdateForm.handleChange}
+                    />
+                    <div className="input-icon">
+                        <Link onClick={() => setEditWebsite(true)} className="setting-edit-button">{user.website ? "edit" : "add"}</Link>
+                    </div>
+                </div>
+                <p className="setting-input-little-description">Portfolio / organization / personal website.</p>
+            </div>
+            <button type="submit">update</button>
+        </form>
+    )
+}
+
+export default UpdateProfile;
