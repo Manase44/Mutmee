a846cb6e9bdf01cf16f88930e85a43fb3c203b4d
Manase44 <fikopersempre@gmail.com>
2024-07-22 03:52:32 +0300

    add the generate access token and refresh token functions in the utils folder

---
+++ server/utils/jwt.config.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+
+export const generateAccessToken = (payload) => {
+    return jwt.sign(payload, process.env.SECRET_KEY, {
+        expiresIn: "10m"
+    })
+};
+
+export const generateRefreshToken = (user) => {
+    return jwt.sign(user, process.env.REFRESH_SECRET_KEY, { 
+        expiresIn: '8h' 
+    })
+};
