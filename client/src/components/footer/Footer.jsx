b19211609b6a43ab2934ee12369f5c2144cd31ba
Manase44 <fikopersempre@gmail.com>
2024-07-22 10:12:00 +0300

    add link to linkedin the the footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer-section">
-      <p>&copy; {new Date().getFullYear()} mutmee by gunga</p>
+      <p>&copy; {new Date().getFullYear()} mutmee by <Link target="_blank" to={"https://www.linkedin.com/in/manasegunga/"}>gunga</Link></p>
     </footer>
   );
 };
