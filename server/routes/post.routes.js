8f95027778765e9a3f08cc4b650ae375c52fe106
Manase44 <fikopersempre@gmail.com>
2024-07-24 02:21:45 +0300

    implement the posting a commen route

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -3,12 +3,14 @@ import createPost from "../controllers/post/addPost.controller.js";
 import deletePost from "../controllers/post/deletePost.controller.js";
 import getAllPosts from "../controllers/post/getAllPosts.controller.js";
 import getPostOfSpecificUser from "../controllers/post/getSingleUserPost.controller.js";
+import addComment from "../controllers/post/addComment.controller.js";
 const route = Router();
 
 route
   .get("/", getAllPosts)
   .get("/user/:id", getPostOfSpecificUser)
   .post("/", createPost)
+  .post("/comment", addComment)
   .delete("/:id", deletePost);
 
 export default route;
