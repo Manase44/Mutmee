71b51b32d0c6ea95caa9c9e0d2db85d8a20a4df0
Manase44 <fikopersempre@gmail.com>
2024-07-24 00:54:04 +0300

    implement the update user profile route

--- client/src/pages/setting/UpdateProfile.jsx
+++ client/src/pages/setting/UpdateProfile.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 import userDetailsStore from "../../store/currentUser.store";
+import { cloud_name, server_url, upload_preset } from "../../../utils/configurations";
 import { useFormik } from "formik";
 import { Link } from "react-router-dom";
+import { Oval } from 'react-loader-spinner';
+import axios from 'axios';
+import toast from "react-simple-toasts";
+import "react-simple-toasts/dist/theme/success.css";
 
 const UpdateProfile = () => {
     const user = userDetailsStore((state) => state.user);
@@ -9,7 +14,61 @@ const UpdateProfile = () => {
     const [editBio, setEditBio] = useState(false);
     const [editRole, setEditRole] = useState(false);
     const [imageInput, setImageInput] = useState(null);
+    const [error, setError] = useState(null)
     const [imageLink, setImageLink] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [generatingImageUrl, setGeneratingImageUrl] = useState();
+    const [userProfile, setUserProfile] = useState();
+
+
+    const uploadImage = async () => {
+        setGeneratingImageUrl(true)
+        const payload = new FormData();
+        payload.append("file", imageInput);
+        payload.append("upload_preset", upload_preset);
+        try {
+          const response = await axios.post(
+            `https://api.cloudinary.com/v1_1/${cloud_name}/upload`,
+            payload,
+          );
+    
+          if (response.statusText === "OK") {
+            setError(null);
+    
+            const secureUrl = response.data.secure_url;
+            setImageLink(
+              secureUrl.replace("/upload", "/upload/w_400/f_auto/q_auto"),
+            );
+          }
+        } catch (error) {
+            console.log(error)
+          setError("someting went wrong while uploading this image!");
+        }finally{
+            setGeneratingImageUrl(false)
+        }
+      };
+
+      const postUserUpdateDetails = async(data) => {
+        setLoading(true)
+        try {
+            const response = await axios.patch(`${server_url}/user/profile`, data, {withCredentials: true})
+            console.log(response)
+            if (response.data.ok) {
+                toast(response.data.message, {
+                    theme: "success",
+                    duration: 4000,
+                    position: "top-right",
+                  });
+                setError(null)
+                setUserProfile(response.data.userProfile)
+            }
+        } catch (error) {
+            console.log(error)
+            setError(error.response.data.message)
+        }finally{
+            setLoading(false)
+        }
+      }
 
     const userDetailsUpdateForm = useFormik({
         initialValues: {
@@ -19,8 +78,14 @@ const UpdateProfile = () => {
             role: user.role,
         },
         onSubmit: (data) => {
-            // data.imageUrl = imageLink;
-            console.log(data)
+            uploadImage();
+            data.imageUrl = imageLink;
+            // console.log("hey",imageLink)
+            if (!error) {
+                postUserUpdateDetails(data)
+            }
+            
+            
         }
     });
     return (
@@ -41,7 +106,8 @@ const UpdateProfile = () => {
                         <input type="file" name="image" id="image" onChange={(e) => { setImageInput(e.target.files[0]) }} accept="image/*" />
                     </div>
                 </div>
-                {imageInput && <span className="edit-photo-selected-image">{imageInput.name}</span>}
+
+                {error ? <p className="edit-photo-selected-image">{error}</p> : imageInput && <span className="edit-photo-selected-image">{imageInput.name}</span>}
             </>
 
             <div className="edit-role-container">
@@ -85,8 +151,7 @@ const UpdateProfile = () => {
                 <div className="input-wrapper">
                     <input
                         type="text"
-                        name="websiteUrl"
-                        id="websiteUrl"
+                        name="website"
                         disabled={!editWebsite}
                         placeholder={!user.website && "No website url!"}
                         value={userDetailsUpdateForm.values.website}
@@ -98,7 +163,18 @@ const UpdateProfile = () => {
                 </div>
                 <p className="setting-input-little-description">Portfolio / organization / personal website.</p>
             </div>
-            <button type="submit">update</button>
+            <button type="submit">{loading ? <span className="updating-button">
+                <Oval
+                    visible={true}
+                    height="20"
+                    width="20"
+                    color="#4fa94d"
+                    ariaLabel="oval-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+                updating...
+            </span> : "update"}</button>
         </form>
     )
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,8 @@ import deleteUser from "../controllers/user/delete.controller.js";
 import getAllusers from "../controllers/user/getUsers.controller.js";
 import getSpecificUser from "../controllers/user/getSpecificUser.controller.js";
 import userLogin from "../controllers/user/userLogin.controller.js";
+import updatingUserProfile from "../controllers/user/updateProfile.js";
+import verifyToken from "../middlewares/auth.js";
 const route = Router();
 
 route
@@ -11,6 +13,7 @@ route
   .get("/:id", getSpecificUser)
   .post("/register", registerUser)
   .post("/login", userLogin)
+  .patch("/profile", verifyToken ,updatingUserProfile)
   .delete("/:id", deleteUser);
 
 export default route;
